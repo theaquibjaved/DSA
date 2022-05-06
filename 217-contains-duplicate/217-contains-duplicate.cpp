d83e303e6da020d6514c9e6ada2173bf15cb3def
Aquib Javed` <96683454+theaquibjaved@users.noreply.github.com>
2022-05-06 22:28:30 +0530

    Time: 129 ms (42.66%), Space: 53.4 MB (21.23%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size()-1; i++){
-            if(nums[i] == nums[i+1])
-                return true;
+        
+        unordered_map<int, int> m;
+        for(auto n : nums) m[n]++;
+        for(auto n : m){
+            if(n.second > 1) return true;
         }
+        
         return false;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // sort(nums.begin(), nums.end()); // O(loog n)
+        // for(int i = 0; i < nums.size()-1; i++){
+        //     if(nums[i] == nums[i+1])
+        //         return true;
+        // }
+        // return false;
         
     }
 };
