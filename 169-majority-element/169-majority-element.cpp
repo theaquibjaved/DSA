1c134f9acc1db4df0ba9a98206b1e941e62b63eb
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-05-07 00:25:57 +0530

    Time: 18 ms (84.70%), Space: 19.6 MB (59.76%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,21 +2,44 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
-        unordered_map<int, int> map;
-        for(auto it : nums) map[it]++;
-        for(auto it : map){
-            if(it.second > nums.size()/2)
-                return it.first;
+        //MOORE'S VOTING ALGORITHM
+        // int count = 0;
+        // int candidate = 0;
+        // for(int it : nums){
+        //     if(count = 0)
+        //         candidate = it;
+        //     if(it == candidate)
+        //         count += 1;
+        //     else 
+        //         count -= 1;
+        // }
+        // return candidate;
+        
+         int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            if(num==candidate) count += 1; 
+            else count -= 1; 
         }
-        return -1;
+
+        return candidate;
         
+        //--------------------
+        // unordered_map<int, int> map;
+        // for(auto it : nums) map[it]++;
+        // for(auto it : map){
+        //     if(it.second > nums.size()/2)
+        //         return it.first;
+        // }
+        // return -1;
+        // // TC: O(N)
+        // // SC: O(N)
         
-        
-        
-        
-        
-        
-        
+        //------------
         
         // int count = 0, n = nums.size() - 1;
         // for(int i = 0; i < n; i++){
