c711e4a0550eed54dfa17c8ed6ea2b4874ce71b7
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-05-09 00:30:44 +0530

    Time: 10 ms (25.99%), Space: 7.8 MB (52.56%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        
+        int m = accounts.size();
+        int n = accounts[0].size();
+        int maxiWealth = 0;
+        
+        for(int i = 0; i < m; i++){
+            int currentCustomerWealth = 0;
+            
+            for(int j = 0; j < n; j++)
+                currentCustomerWealth += accounts[i][j];
+            
+            maxiWealth = max(maxiWealth, currentCustomerWealth);
+        }
+        
+        
+        return maxiWealth;
+    }
+};
