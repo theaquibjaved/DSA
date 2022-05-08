d66b236a81da41c2d9d719c925df2e75ee1f59f2
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-05-09 00:46:57 +0530

    Time: 26 ms (21.46%), Space: 11.3 MB (12.63%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -5,9 +5,9 @@ public:
         int res = 0;
         int n = mat.size();
         for (int i=0; i<n; i++) {
-            res += mat[i][i]; // Primary diagonals are row = column! 
-            res += mat[n-1-i][i]; // Secondary diagonals are row + column = n-1!
+            res += mat[i][i]; 
+            res += mat[n-1-i][i]; 
         }
-        return n % 2 == 0 ? res : res - mat[n/2][n/2]; // if n is a odd number, that mean we have added the center element twice!
+        return n % 2 == 0 ? res : res - mat[n/2][n/2]; 
     }
 };
