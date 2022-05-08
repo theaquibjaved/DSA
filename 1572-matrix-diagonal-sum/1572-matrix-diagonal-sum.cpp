411e4d4519239a139365d35919db93bdde86a106
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-05-09 00:46:05 +0530

    Time: 37 ms (5.78%), Space: 11.2 MB (93.00%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        
+        int res = 0;
+        int n = mat.size();
+        for (int i=0; i<n; i++) {
+            res += mat[i][i]; // Primary diagonals are row = column! 
+            res += mat[n-1-i][i]; // Secondary diagonals are row + column = n-1!
+        }
+        return n % 2 == 0 ? res : res - mat[n/2][n/2]; // if n is a odd number, that mean we have added the center element twice!
+    }
+};
