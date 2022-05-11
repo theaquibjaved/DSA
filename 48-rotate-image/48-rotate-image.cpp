3bc554b201ce46e101e9dfe5096c70799cfb53da
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-05-11 16:55:09 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (34.40%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -3,16 +3,16 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         
         int n = matrix.size();
-        vector < vector < int >> rotated(n, vector < int > (n, 0));
                 
+        //transpose
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++)
-                rotated[j][n-i-1] = matrix[i][j];
+            for(int j = 0; j < i; j++)
+                swap(matrix[i][j], matrix[j][i]);
         }
         
+        //reverse
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++)
-                matrix[i][j] = rotated[i][j];
+                reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
