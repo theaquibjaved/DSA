2ba5d2c42ce85aac365083a33a151b1484240fe0
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-06-23 13:14:44 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -12,7 +12,9 @@ class Solution{
     public:
     void solve(int i, int j, vector < vector < int >> & a, int n, vector < string > & ans, string move,
     vector < vector < int >> & vis, int di[], int dj[]) {
-    if (i == n - 1 && j == n - 1) {
+    
+    bool reachesBoundary = i == n - 1 && j == n - 1;
+    if (reachesBoundary) {
       ans.push_back(move);
       return;
     }
@@ -20,6 +22,8 @@ class Solution{
     for (int ind = 0; ind < 4; ind++) {
       int nexti = i + di[ind];
       int nextj = j + dj[ind];
+      
+      bool inBou
       if (nexti >= 0 && nextj >= 0 && nexti < n && nextj < n && !vis[nexti][nextj] && a[nexti][nextj] == 1) {
         vis[i][j] = 1;
         solve(nexti, nextj, a, n, ans, move + dir[ind], vis, di, dj);
