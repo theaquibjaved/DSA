e882ec21c5f9bc781ef0e5e2880171a14d1fe72f
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-06-28 22:42:29 +0530

    Time: 8 ms (7.35%), Space: 6.3 MB (80.01%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st; 
+        for(auto it: s) {
+            if(it=='(' || it=='{' || it == '[') st.push(it); 
+            else {
+                if(st.size() == 0) return false; 
+                char ch = st.top(); 
+                st.pop(); 
+                bool balancedBracket = (it == ')' and ch == '(') or  (it == ']' and ch == '[') or (it == '}' and ch == '{');
+                if(balancedBracket) continue;
+                else return false;
+            }
+        }
+        return st.empty(); 
+    }
+};
