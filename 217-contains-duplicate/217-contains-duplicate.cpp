f479871d9e2a1b039ff2a09bd34d3e7db26eafeb
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-07-02 20:02:57 +0530

    Time: 176 ms (22.69%), Space: 46.6 MB (78.93%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         
-                return nums.size() > set<int>(nums.begin(), nums.end()).size();        
+//          return nums.size() > set<int>(nums.begin(), nums.end()).size();        
 
-        // TC: O(N)
-        // SC: O(N)
+//         // TC: O(N)
+//         // SC: O(N)
         
         
         
@@ -14,12 +14,12 @@ public:
         
         
         
-        // sort(nums.begin(), nums.end()); // O(loog n)
-        // for(int i = 0; i < nums.size()-1; i++){
-        //     if(nums[i] == nums[i+1])
-        //         return true;
-        // }
-        // return false;
+        sort(nums.begin(), nums.end()); // O(loog n)
+        for(int i = 0; i < nums.size()-1; i++){
+            if(nums[i] == nums[i+1])
+                return true;
+        }
+        return false;
         
     }
 };
