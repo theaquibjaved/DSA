a98886ccf3f3b1c390d86422ba24d5c56e76962f
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-07-04 18:42:14 +0530

    Time: 26 ms (79.99%), Space: 16.4 MB (69.33%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -1,34 +1,57 @@
 class MinStack {
     
-    stack<pair<int, int>> st;
+    stack<long long> st;
+    long long mini;
 public:
     MinStack() {
-        
+        while(!st.empty())
+            st.pop();
+         mini = INT_MAX;
     }
     
     void push(int x) {
-      int min;
-      if (st.empty()) {
-        min = x;
-      } else {
-        min = std::min(st.top().second, x);
-      }
-      st.push({x,min});
+        long long val = x;
+        //int mini;
+        
+        if(st.empty()){
+            mini = val;
+            st.push(val);
+        }
+        else{
+            if(val < mini){
+                st.push(2 * val * 1LL - mini);
+                mini = val;
+            }
+            else{
+                st.push(val);
+            }
+        }     
     }
     
     void pop() {
+        if(st.empty()) return;
+        
+        long long el = st.top();
         st.pop();
+        
+        if(el < mini){
+            mini = 2 * mini - el;
+        }
     }
     
     int top() {
-        return st.top().first;
+        if(st.empty()) return -1;
+        
+        long long el = st.top();
+        
+        if(el < mini)
+            return mini;
+        
+        return el;
     }
     
-    // int getMin() {
-    //     return st.top().second();
-    // }
     int getMin() {
-        return st.top().second;
+        return mini;   
     }
 };
 
