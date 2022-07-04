b22f7e829cf90a57c41b54de811d4603ffd1d751
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-07-04 22:14:03 +0530

    Time: 9 ms (66.82%), Space: 13.1 MB (79.25%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -26,9 +26,9 @@ public:
                 for(int i = 0; i < 4; ++i){
                     int nx = x + dx[i], ny = y + dy[i];
                     
-                    bool isValid = nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] != 1;
+                    bool xyz = nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] != 1;
                     
-                    if(!isValid) {
+                    if(!xyz) {
                          grid[nx][ny] = 2;
                         rotten.push({nx, ny});
                     }
