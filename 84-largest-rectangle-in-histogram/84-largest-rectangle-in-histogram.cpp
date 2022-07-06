71fccdd1a409a490a23f6c079deba34e24d4afc3
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-07-06 10:54:18 +0530

    Time: 283 ms (46.19%), Space: 87.5 MB (24.25%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
+    vector<int> prevSmallerElement(vector<int>& heights){
         int n = heights.size();
         stack<int> st;
-        int leftSmall[n], rightSmall[n];
+        vector<int> pse(n);
         
         //for left smaller:
         for(int i = 0; i < n; i++){
@@ -11,31 +11,76 @@ public:
                 st.pop();
             
             if(!st.empty())
-                leftSmall[i] = st.top() + 1;
+                pse[i] = st.top() + 1;
             
             else
-                leftSmall[i] = 0;
+                pse[i] = 0;
                 
             st.push(i);
         }
+        return pse;
+    }
+    
+    vector<int> nextSmallerElement(vector<int>& heights){
+        int n = heights.size();
+        stack<int> st;
+        vector<int> nse(n);
         
-        //clear the stack to re-use:
-        while(!st.empty())
-            st.pop();
-        
-        //for right smaller:
         for(int i = n-1; i >= 0; i--){
             while(!st.empty() && heights[st.top()] >= heights[i])
                 st.pop();
             
             if(!st.empty())
-                rightSmall[i] = st.top() - 1;
+                nse[i] = st.top() - 1;
             
             else
-                rightSmall[i] = n - 1;
+                nse[i] = n - 1;
                 
             st.push(i);
         }
+        return nse;
+    }
+    
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        //stack<int> st;
+        vector<int> leftSmall(n);
+        vector<int> rightSmall(n);
+        
+        leftSmall = prevSmallerElement(heights);
+        rightSmall = nextSmallerElement(heights);
+        
+//         //for left smaller:
+//         for(int i = 0; i < n; i++){
+//             while(!st.empty() && heights[st.top()] >= heights[i])
+//                 st.pop();
+            
+//             if(!st.empty())
+//                 leftSmall[i] = st.top() + 1;
+            
+//             else
+//                 leftSmall[i] = 0;
+                
+//             st.push(i);
+//         }
+        
+        //clear the stack to re-use:
+        // while(!st.empty())
+        //     st.pop();
+        
+//         //for right smaller:
+//         for(int i = n-1; i >= 0; i--){
+//             while(!st.empty() && heights[st.top()] >= heights[i])
+//                 st.pop();
+            
+//             if(!st.empty())
+//                 rightSmall[i] = st.top() - 1;
+            
+//             else
+//                 rightSmall[i] = n - 1;
+                
+//             st.push(i);
+//         }
         
         //calculate the area
         int maxArea = 0, currArea, width;
