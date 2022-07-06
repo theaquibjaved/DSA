3cc33d8fed19c5fd5474de722665022878e450c9
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-07-06 17:20:24 +0530

    Time: 8 ms (58.48%), Space: 7.3 MB (69.54%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,45 +1,44 @@
 class Solution {
 public:
     string reverseWords(string str) {
-    if(str == "" || str == " ")
-    {
-        return "";
-    }
+        int n = str.length();
+        string ans;
+        int i = 0;
 
-    string ans;
-
-    int start = str.length() - 1;
-
-    while(start >= 0)
-    {
-        
-        // Skip multiple spaces
-        if(str[start] == ' ')
+        while (i < n) 
         {
-            start--;
-        }
-        else
-        {
-            
-            // Add space between words
-            if(ans.length() > 0)
+            //int j = i;
+
+            // Skip multiple spaces
+            while (i < n && str[i] == ' ') 
             {
-                ans.push_back(' ');
+                i++;
             }
 
-            int j = start;
+            string currentWord;
 
-            while(j >= 0 && str[j] != ' ')
+            // Get the current word
+            while (i < n && str[i] != ' ') 
             {
-                j--;
+                currentWord.push_back(str[i]);
+                i++;
             }
 
-            // add current word to ans
-            ans.append(str.substr(j+1, start-j));
-            start = j;
-        }
-    }
+            // add current word in the ans with a space
+            if (currentWord.length() != 0) 
+            {
+                ans.insert(0, currentWord + " ");
+            }
 
-    return ans;
+            i = i + 1;
+        }
+
+        if (ans.length() == 0) 
+        {
+            return ans;
+        }
+
+        // remove the last space
+        return ans.substr(0, ans.length() - 1);
     }
 };
