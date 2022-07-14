891a6ad97e2227e8512edc2bde5cc727216f2246
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-07-14 16:17:25 +0530

    Time: 41 ms (48.34%), Space: 27.7 MB (41.01%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -11,54 +11,39 @@
  */
 class Solution {
 public:
+    int maxi = INT_MIN;
+    int findMaxPath(TreeNode* node){
+        if(!node)
+            return 0;
+        
+        int leftMaxPath = max(0, findMaxPath(node->left));
+        int rightMaxPath = max(0, findMaxPath(node->right));
+        
+        int temp = (leftMaxPath + rightMaxPath) + node->val;
+        maxi = max(maxi, temp);
+        
+        return max(leftMaxPath, rightMaxPath) + node->val;
+    }
     
-//     int findMaxPath(TreeNode* node, int maxi){
-//         if(!node)
-//             return 0;
-        
-//         int leftMaxPath = max(0, findMaxPath(node->left, maxi));
-//         int rightMaxPath = max(0, findMaxPath(node->right, maxi));
-        
-//         int temp = (leftMaxPath + rightMaxPath) + node->val;
-//         maxi = max(maxi, temp);
-        
-//         return max(leftMaxPath, rightMaxPath) + node->val;
-//     }
+    int maxPathSum(TreeNode* root) {
+
+        findMaxPath(root);
+        return maxi;
+    }
+
     
-//     int maxPathSum(TreeNode* root) {
-//         int maxi = INT_MIN;
-//         findMaxPath(root, maxi);
-//         return maxi;
-//     }
-    // int solve(TreeNode* root,int &res)
+    //  int max_sum=INT_MIN;
+    // int max_gain(TreeNode* root)
     // {
-    //     // Base Case 
-    //     if(root==NULL) return NULL;
-    //     int ls = solve(root->left,res);
-    //     int rs = solve(root->right,res);
-    //     int temp = max(max(ls,rs)+root->val,root->val);
-    //     int ans = max(temp,ls+rs+root->val);
-    //     res = max(res,ans);
-    //     return temp;
+    //     if(!root)return 0;
+    //     int l=max(max_gain(root->left),0);
+    //     int r=max(max_gain(root->right),0);
+    //     int new_price=root->val+l+r;
+    //     max_sum=max(max_sum,new_price);
+    //     return root->val+max(l,r);
     // }
     // int maxPathSum(TreeNode* root) {
-    //     int res = INT_MIN;
-    //     solve(root,res);
-    //     return res;
+    //     max_gain(root);
+    //     return max_sum;
     // }
-    
-     int max_sum=INT_MIN;
-    int max_gain(TreeNode* root)
-    {
-        if(!root)return 0;
-        int l=max(max_gain(root->left),0);
-        int r=max(max_gain(root->right),0);
-        int new_price=root->val+l+r;
-        max_sum=max(max_sum,new_price);
-        return root->val+max(l,r);
-    }
-    int maxPathSum(TreeNode* root) {
-        max_gain(root);
-        return max_sum;
-    }
 };
