aa267924aa239b5c61700d6509d62ee3b7445e80
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-07-18 18:56:47 +0530

    Time: 30 ms (63.15%), Space: 26.4 MB (28.28%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,26 +11,30 @@
  */
 class Solution {
 public:
+    TreeNode * constructTree(vector < int > & preorder, int preStart, int preEnd, vector 
+ < int > & inorder, int inStart, int inEnd, map < int, int > & mp) {
+  if (preStart > preEnd || inStart > inEnd) return NULL;
 
+  TreeNode * root = new TreeNode(preorder[preStart]);
+  int elem = mp[root -> val];
+  int nElem = elem - inStart;
 
-int preInd=0;
-TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
-    return createTree(preorder,inorder,0,inorder.size() - 1);
+  root -> left = constructTree(preorder, preStart + 1, preStart + nElem, inorder,
+  inStart, elem - 1, mp);
+  root -> right = constructTree(preorder, preStart + nElem + 1, preEnd, inorder, 
+  elem + 1, inEnd, mp);
+
+  return root;
 }
-TreeNode* createTree(vector<int>& preorder, vector<int>& inorder,int start, int end){
-    if(start > end){
-        return NULL;
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int preStart = 0, preEnd = preorder.size() - 1;
+  int inStart = 0, inEnd = inorder.size() - 1;
+
+  map < int, int > mp;
+  for (int i = inStart; i <= inEnd; i++) {
+    mp[inorder[i]] = i;
+  }
+
+  return constructTree(preorder, preStart, preEnd, inorder, inStart, inEnd, mp);
     }
-    TreeNode* node=new TreeNode(preorder[preInd++]);
-    int pos;
-    for(int i=start;i<=end;i++){
-        if(inorder[i]==node->val){
-            pos=i;
-            break;
-        }
-    }
-    node->left =createTree(preorder, inorder,start,pos-1);
-    node->right =createTree(preorder, inorder, pos+1,end);
-    return node;
-}
 };
