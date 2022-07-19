bf8d7ced25c38eeb462eaf930b510ad30ce23d31
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-07-19 10:59:28 +0530

    Time: 53 ms (25.97%), Space: 26 MB (75.55%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -49,7 +49,7 @@ public:
         if(x > y || a > b)return nullptr;
         TreeNode *node = new TreeNode(po[b]);
         int SI = x;  
-        while(node->val != in[SI])SI++;
+         while(node->val != in[SI])SI++;
         node->left  = Tree(in,x,SI-1,po,a,a+SI-x-1);
         node->right = Tree(in,SI+1,y,po,a+SI-x,b-1);
         return node;
