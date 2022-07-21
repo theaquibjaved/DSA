cb4b572331447c07b4a27678e9662db210e73104
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-07-21 10:43:10 +0530

    Time: 29 ms (48.51%), Space: 24.1 MB (86.62%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -12,66 +12,40 @@
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
+        TreeNode* cur = root;
         int count = 0;
- 
-    int ksmall = INT_MIN; // store the Kth smallest
-    TreeNode *curr = root; // to store the current node
- 
-    while (curr != NULL)
-    {
-        // Like Morris traversal if current does
-        // not have left child rather than printing
-        // as we did in inorder, we will just
-        // increment the count as the number will
-        // be in an increasing order
-        if (curr->left == NULL)
-        {
-            count++;
- 
-            // if count is equal to K then we found the
-            // kth smallest, so store it in ksmall
-            if (count==k)
-                ksmall = curr->val;
- 
-            // go to current's right child
-            curr = curr->right;
-        }
-        else
-        {
-            // we create links to Inorder Successor and
-            // count using these links
-            TreeNode *pre = curr->left;
-            while (pre->right != NULL && pre->right != curr)
-                pre = pre->right;
- 
-            // building links
-            if (pre->right==NULL)
-            {
-                //link made to Inorder Successor
-                pre->right = curr;
-                curr = curr->left;
-            }
- 
-            // While breaking the links in so made temporary
-            // threaded tree we will check for the K smallest
-            // condition
-            else
-            {
-                // Revert the changes made in if part (break link
-                // from the Inorder Successor)
-                pre->right = NULL;
- 
+        int ksmall = INT_MIN;
+        
+        while(cur){
+            if(cur->left == NULL){
                 count++;
- 
-                // If count is equal to K then we found
-                // the kth smallest and so store it in ksmall
-                if (count==k)
-                    ksmall = curr->val;
- 
-                curr = curr->right;
+                if(count == k)
+                    ksmall = cur->val;
+                    
+                cur = cur->right;
+            }
+            else{
+                TreeNode* prev = cur->left;
+                
+                while(prev->right != NULL && prev->right != cur)
+                    prev = prev->right;
+                
+                if(prev->right == NULL){
+                    prev->right = cur;
+                    cur = cur->left;
+                }
+                else{
+                    prev->right = NULL;
+                    
+                    count++;
+                    if(count == k)
+                        ksmall = cur->val;
+                    
+                    cur = cur->right;
+                }
             }
         }
-    }
-    return ksmall;
+        return ksmall;
+
     }
 };
