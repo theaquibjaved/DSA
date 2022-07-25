3b01439f9c28ab408743c834d557f47b68a47ea4
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-07-25 20:33:41 +0530

    Time: 75 ms (33.81%), Space: 36.9 MB (62.68%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,21 +9,51 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTIterator {
+    stack<TreeNode*> s;
+    TreeNode* node;
+    bool forward;
+public:
+    BSTIterator(TreeNode *root, bool forward) : node(root), forward(forward) {};
+    bool hasNext() {
+        return node != nullptr || !s.empty();
+    }
+    int next() {
+        while (node || !s.empty()) {
+            if (node) {
+                s.push(node);
+                node = forward ? node->left : node->right;
+            }
+            else {
+                node = s.top();
+                s.pop();
+                int nextVal = node->val;
+                node = forward ? node->right : node->left;
+                return nextVal;
+            }
+        }
+
+        return -1;  // never reach & not necessary
+    }
+};
 class Solution {
 public:
-     bool findTarget(TreeNode* root, int k) {
-        return dfs(root, root,  k);
-    }
-    
-    bool dfs(TreeNode* root,  TreeNode* cur, int k){
-        if(cur == NULL)return false;
-        return search(root, cur, k - cur->val) || dfs(root, cur->left, k) || dfs(root, cur->right, k);
-    }
-    
-    bool search(TreeNode* root, TreeNode *cur, int value){
-        if(root == NULL)return false;
-        return (root->val == value) && (root != cur) 
-            || (root->val < value) && search(root->right, cur, value) 
-                || (root->val > value) && search(root->left, cur, value);
+    bool findTarget(TreeNode* root, int k) {
+        if (!root) return false;
+        BSTIterator l(root, true);
+        BSTIterator r(root, false);
+        for (int i = l.next(), j = r.next(); i < j;) {
+            int sum = i + j;
+            if (sum == k) {
+                return true;
+            }
+            else if (sum < k) {
+                i = l.next();
+            }
+            else {
+                j = r.next();
+            }
+        }
+        return false;
     }
 };
