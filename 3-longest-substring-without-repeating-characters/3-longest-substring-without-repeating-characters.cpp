884b66e395d9be2ab1e8e96e28ea104849e4891b
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-07-27 18:57:30 +0530

    Time: 19 ms (66.73%), Space: 8.4 MB (57.99%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
-    // MOST OPTIMAL:
     int lengthOfLongestSubstring(string s) {
-        
-      vector<int> mpp(256, -1);
+         vector<int> mpp(256, -1);
 
       int left = 0, right = 0;
       int n = s.size();
@@ -11,7 +9,7 @@ public:
       while (right < n) {
         if (mpp[s[right]] != -1)
           left = max(mpp[s[right]] + 1, left);
-
+        
         mpp[s[right]] = right;
 
         len = max(len, right - left + 1);
@@ -20,25 +18,3 @@ public:
       return len;
     }
 };
-// --------------------------------------------
-//BETTER:
-// int lengthOfLongestSubstring(string str) {
-        
-//        int maxans = INT_MIN;
-//   unordered_set < int > set;
-//   int l = 0;
-//   for (int r = 0; r < str.length(); r++) // outer loop for traversing the string
-//   {
-//     if (set.find(str[r]) != set.end()) //if duplicate element is found
-//     {
-//       while (l < r && set.find(str[r]) != set.end()) {
-//         set.erase(str[l]);
-//         l++;
-//       }
-//     }
-//     set.insert(str[r]);
-//     maxans = max(maxans, r - l + 1);
-//   }
-//   return maxans;
-//     }
-// };
