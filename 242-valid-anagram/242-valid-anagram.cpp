449ecc9c2aec0c146ff952a4f190291e6ebd079a
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-08-01 13:24:43 +0530

    Time: 23 ms (22.71%), Space: 7.4 MB (45.20%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size())
+        
+        int n = s.size();
+        int m = t.size();
+        
+        if(n != m)
             return false;
+        
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
-        return (s == t);
+        
+        for(int i = 0; i < n; i++){
+            if(s[i] != t[i])
+                return false;
+        }
+            
+        
+        return true;
     }
 };
