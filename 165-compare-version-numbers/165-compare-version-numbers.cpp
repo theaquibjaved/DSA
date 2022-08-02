a9afdfa16736f18e44cf81abd4f17e73ee59002a
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-08-02 16:02:46 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (28.36%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-    int compareVersion(string v1, string v2) {
+    int compareVersion(string version1, string version2) {
+        int n = version1.length();
+        int m = version2.length();
+        
         int vnum1 = 0, vnum2 = 0;
- 
-    // loop until both string are
-    // processed
-    for (int i = 0, j = 0; (i < v1.length()
-                            || j < v2.length());) {
-        // storing numeric part of
-        // version 1 in vnum1
-        while (i < v1.length() && v1[i] != '.') {
-            vnum1 = vnum1 * 10 + (v1[i] - '0');
+        int i = 0, j = 0;
+        
+        while(i < n || j < m){
+            
+            while(i < n && version1[i] != '.'){
+                vnum1 = vnum1*10 + (version1[i] - '0');
+                i++;
+            }
+            
+            while(j < m && version2[j] != '.'){
+                vnum2 = vnum2*10 + (version2[j] - '0');
+                j++;
+            }
+            
+            if(vnum1 < vnum2)
+                return -1;
+            if(vnum1 > vnum2)
+                return 1;
+            
+            vnum1 = vnum2 = 0; //for next level
             i++;
-        }
- 
-        // storing numeric part of
-        // version 2 in vnum2
-        while (j < v2.length() && v2[j] != '.') {
-            vnum2 = vnum2 * 10 + (v2[j] - '0');
             j++;
         }
- 
-        if (vnum1 > vnum2)
-            return 1;
-        if (vnum2 > vnum1)
-            return -1;
- 
-        // if equal, reset variables and
-        // go for next numeric part
-        vnum1 = vnum2 = 0;
-        i++;
-        j++;
-    }
-    return 0;
+        
+        return 0;
     }
 };
