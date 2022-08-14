89f45c249688d35f48df901226f72454682d5578
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-08-14 22:48:43 +0530

    Time: 25 ms (53.85%), Space: 15.4 MB (9.43%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -11,8 +11,8 @@ public:
         
         for (int num : nums)
             numMap[num]++;
-        for (auto it = numMap.begin(); it != numMap.end(); it++) 
-            buckets[it->second].push_back(it->first);
+        for (auto it : numMap) 
+            buckets[it.second].push_back(it.first);
         
         for (int i = buckets.size()-1; i >= 0; i--) {
             for (int j = 0; j < buckets[i].size(); j++) {
