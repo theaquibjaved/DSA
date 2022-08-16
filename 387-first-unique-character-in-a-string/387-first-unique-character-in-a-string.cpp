fcfeef8b9ae3ca6b104984c5846a905e0d6cfe65
Aquib Javed <96683454+theaquibjaved@users.noreply.github.com>
2022-08-16 09:09:25 +0530

    Time: 55 ms (52.29%), Space: 10.8 MB (15.65%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -5,7 +5,7 @@ public:
         for (char& c : s) {
             m[c]++;
         }
-        for (int i = 0; i < s.size(); i++) {
+        for (int i = 0; i <= s.size(); i++) {
             if (m[s[i]] == 1) return i;
         }
         return -1;
